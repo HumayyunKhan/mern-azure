98945f3d6db8ed9500116c6ba0fe4567ae98566f
Humayyun Khan <humayyunatwork@gmail.com>
2023-10-23 19:47:33 +0500

    server port fixes

--- server.js
+++ server.js
@@ -9,43 +9,45 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("Database is connected..."))
-  .catch((err) => console.log(err));
+// mongoose
+//   .connect(process.env.MONGO_URI)
+//   .then(() => console.log("Database is connected..."))
+//   .catch((err) => console.log(err));
 
-//db schema
-const userSchema = mongoose.Schema({
-  name: String,
-  lastName: String,
-});
+// //db schema
+// const userSchema = mongoose.Schema({
+//   name: String,
+//   lastName: String,
+// });
 
 //db model
-const User = new mongoose.model("User", userSchema);
+// const User = new mongoose.model("User", userSchema);
 
 app.get("/get-users", (req, res) => {
-  User.find()
-    .then((users) => res.json(users))
-    .catch((err) => console.log(err));
+  res.send("helllo")
+  // User.find()
+  //   .then((users) => res.json(users))
+  //   .catch((err) => console.log(err));
 });
 
 app.post("/create", (req, res) => {
   //save to mongodb and send response
-  const newUser = new User({
-    name: req.body.name,
-    lastName: req.body.lastName,
-  });
+  // const newUser = new User({
+  //   name: req.body.name,
+  //   lastName: req.body.lastName,
+  // });
 
-  newUser
-  .save()
-  .then((user) => res.json(user))
-  .catch((err) => console.log(err));
+  // newUser
+  // .save()
+  // .then((user) => res.json(user))
+  // .catch((err) => console.log(err));
+  res.send("HELLLO WORLDE CREATED")
 });
 
 app.use(express.static("./client/build"))
 app.get("*",(req,res)=>{
   res.sendFile(path.resolve(__dirname,"client","build","index.html"))
 })
-app.listen(port, () => {
+app.listen(3001, () => {
   console.log(`Server is running on post ${port}`);
 });
